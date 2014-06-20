5c6aa1dce362a309906cee954c861ec023a3c01b
Christophe Serafin <christophe.serafin@gmail.com>
2014-06-20 09:54:16 -0700

    removed Singleton dependency on NURESTUser and added method to convert NUUser/NURESTUser

--- nurestuser.py
+++ nurestuser.py
@@ -83,3 +83,17 @@ class NURESTUser(NURESTBasicUser):
         """ Fetch enterprises """
 
         return self._enterprises_fetcher.fetch_entities()
+
+    def to_NUUser(self):
+        """ Convert to NUUser """
+
+        from . import NUUser
+        user = NUUser()
+
+        for attribute in self.get_attributes():
+
+            if hasattr(user, attribute.local_name):
+                value = getattr(self, attribute.local_name)
+                setattr(user, attribute.local_name, value)
+
+        return user
--- nuuser.py
+++ nuuser.py
@@ -41,3 +41,19 @@ class NUUser(NURESTObject):
         """ Provides restname """
 
         return u"user"
+
+    def to_NURESTUser(self):
+        """ convert NUUser to NURESTUser """
+
+        from . import NURESTUser
+        user = NURESTUser()
+
+        for attribute in self.get_attributes():
+
+            if hasattr(user, attribute.local_name):
+                value = getattr(self, attribute.local_name)
+                setattr(user, attribute.local_name, value)
+
+        return user
+
+        
