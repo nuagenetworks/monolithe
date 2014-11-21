c256ea0516f4629c8d1754414ce3fe8e6590c427
Christophe Serafin <christophe.serafin@gmail.com>
2014-11-20 19:42:32 -0700

    Automatic push

--- vsdk_V3_0/nuvsdsession.py
+++ vsdk_V3_0/nuvsdsession.py
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
 from bambou import NURESTLoginController
-from . import NURESTUser
+from nurestuser import NURESTUser
 from .utils import *
 
 
