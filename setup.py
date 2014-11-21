1423caf93930549b1cd1779d1dee3a0a674293b1
Antoine Mercadal <antoine.mercadal@gmail.com>
2014-11-21 13:26:56 -0800

    Remove generated part

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='vsdk_api',
-    version='V3_0_1',
+    name='vsdk',
+    version='1.0.0',
     url='http://www.nuagenetworks.net/',
     author='Christophe Serafin, Aravind Ganesan',
     author_email='christophe.serafin@alcatel-lucent.com, aravind.ganesan@alcatel-lucent.com',
-    packages=['vsdk_V3_0', 'vsdk_V3_0.autogenerates', 'vsdk_V3_0.fetchers'],
-    description='VSD Python SDK for API V3_0',
+    packages=['vsdk', 'vsdk.autogenerates', 'vsdk.fetchers'],
+    description='VSD Python SDK for API',
     long_description=open('README.md').read(),
     install_requires=[line for line in open('requirements.txt')],
 )
