9148a7980a3578b4cd5353ab48136c9691c80e1a
Christophe Serafin <christophe.serafin@gmail.com>
2014-12-17 17:15:27 -0800

    Replaced sdk vanilla sources in vanilla/sdk

---
+++ vanilla/sdk/vsdk/autogenerates/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import os
+import inspect
+import sys
+
+classes = []
+
+for module in os.listdir(os.path.dirname(__file__)):
+    if module.startswith('nu') and module.endswith('.py'):
+
+        module_name = module[:-3]
+        package_name = __name__
+
+        import_name = '%s.%s' % (package_name, module_name)
+
+        # Import module
+        __import__(import_name)
+
+        members = inspect.getmembers(sys.modules[import_name], lambda member: inspect.isclass(member) and member.__module__.startswith(package_name))
+        names = [member[0] for member in members]
+
+        if len(names) > 0:
+            # Execute import with classnames
+            exec('from %s import %s' % (module_name, ', '.join(names)), globals(), locals())
+
+        classes.extend(names)
+
+__all__ = classes
