355940a3b5fab7a9ebff04fa0e91deaf34d02bd2
Christophe Serafin <christophe.serafin@gmail.com>
2015-02-13 15:22:01 -0800

    Added tests on singular and plural names

---
+++ generator/src/tests/utils/test_plural_name.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+from src.lib import Utils
+
+
+class GetPluralNameTest(TestCase):
+    """ Test for get_plural_name function
+
+    """
+    def assertPluralNameEquals(self, singular_name, plural_name):
+        """ Check that the remote name is well converted to
+            the python name
+
+        """
+        self.assertEqual(Utils.get_plural_name(singular_name), plural_name)
+
+    def test_get_plural_name(self):
+        """ Compute plural names properly
+
+        """
+        self.assertPluralNameEquals('enterprise', 'enterprises')
+        self.assertPluralNameEquals('enterprises', 'enterprises')
+        self.assertPluralNameEquals('Enterprise', 'Enterprises')
+        self.assertPluralNameEquals('Enterprises', 'Enterprises')
+        self.assertPluralNameEquals('policy', 'policies')
+        self.assertPluralNameEquals('qos', 'qos')
