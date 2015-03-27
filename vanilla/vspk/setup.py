8fd00bd177d44a1e6f648760ffc175e24fe5d02c
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-03-27 11:52:33 -0700

    automatically generates the packages list

--- vanilla/vspk/setup.py
+++ vanilla/vspk/setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
+import os
+
+packages = ['vspk', 'vspk.vsdk']
+vsdks_path = "./vspk/vsdk"
+
+for item in os.listdir(vsdks_path):
+    if os.path.isfile("%s/%s" % (vsdks_path, item)):
+        continue
+
+    packages.append("vspk.vsdk.%s" % item)
+    packages.append("vspk.vsdk.%s.fetchers" % item)
+    packages.append("vspk.vsdk.%s.autogenerates" % item)
 
 setup(
     name='vspk',
-    packages=['vspk'],
+    packages=packages,
     include_package_data=True,
     version='0.1',
     description='VSPK',
