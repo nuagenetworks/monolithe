d99ac5900a09e9a8f9f9d6f3d3410dd556cfdbf3
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-05-06 17:22:02 -0700

    improve console out

--- monolithe/specsvalidator/validator.py
+++ monolithe/specsvalidator/validator.py
@@ -50,34 +50,39 @@ class Validator:
         attribute_validation_count   = 0
 
         for entity, validation in sorted(self._report.items()):
-            print "Specification Validation Report for entity '%s'" % entity
-            print ""
+            errored = False
+
+            out = "\n\033[95mSpecification Validation Report for entity '%s'\033[0m\n\n" % entity
 
             if not validation:
-                print " - missing candidate file."
+                errored = True
+                out = "%s\n - missing candidate file." % out
                 missing_candidate_count = missing_candidate_count + 1
-                continue
 
-            if not len(validation.attribute_errors):
-                print " - attributes specification:  \033[92mCONFORM\033[0m"
             else:
-                print " - attributes specification:  \033[91mNOT CONFORM\033[0m"
-                for error in validation.attribute_errors:
-                    attribute_validation_count = attribute_validation_count + 1
-                    print "     %s" % error
-                print ""
 
-            if not len(validation.parent_api_errors):
-                print " - parent apis specification: \033[92mCONFORM\033[0m"
-            else:
-                print " - parent apis specification: \033[91mNOT CONFORM\033[0m"
-                for error in validation.parent_api_errors:
-                    parent_apis_validation_count = parent_apis_validation_count + 1
-                    print "     %s" % error
-                print ""
+                if len(validation.attribute_errors):
+                    errored = True
+                    out = "%s\033[91mattribute conformity errors\033[0m" % out
+                    for error in validation.attribute_errors:
+                        attribute_validation_count = attribute_validation_count + 1
+                        out = "%s\n     %s" % (out, error)
+                    out = "%s\n\n" % out
 
-            print ""
-            print ""
+                if len(validation.parent_api_errors):
+                    errored = True
+                    out = "%s\033[91mapi conformity errors\033[0m" % out
+                    for error in validation.parent_api_errors:
+                        parent_apis_validation_count = parent_apis_validation_count + 1
+                        out = "%s\n     %s" % (out, error)
+                    out = "%s\n\n" % out
 
-        print "\nmissing apis: %d, api validation errors: %d, attribute validation errors: %d" % (missing_candidate_count, parent_apis_validation_count, attribute_validation_count)
+            if errored:
+                print out
+
+        missing_candidates = "\033[92m0\033[0m"  if not missing_candidate_count else "\033[91m%d\033[0m" % missing_candidate_count
+        validation_errors = "\033[92m0\033[0m"  if not parent_apis_validation_count else "\033[91m%d\033[0m" % parent_apis_validation_count
+        api_errors = "\033[92m0\033[0m"  if not attribute_validation_count else "\033[91m%d\033[0m" % attribute_validation_count
+
+        print "missing apis: %s, api validation errors: %s, attribute validation errors: %s\n" % (missing_candidates, validation_errors, api_errors)
 
