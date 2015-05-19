c583814d4d0e845911734192b83160ff12932b6d
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-05-19 10:47:03 -0700

    remove the warning fix again...

--- monolithe/__init__.py
+++ monolithe/__init__.py
@@ -2,8 +2,5 @@
 
 __all__ = ['Command', 'APIValidator']
 
-import requests
-requests.packages.urllib3.disable_warnings()
-
 from .command import Command
 from .specsvalidator.apivalidator import APIValidator
