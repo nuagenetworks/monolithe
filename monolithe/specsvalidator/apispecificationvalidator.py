10a8a684865c3438a62ebaa9c92ca9addef03f38
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-05-20 15:15:39 -0700

    refactor api validator

--- monolithe/specsvalidator/apispecificationvalidator.py
+++ monolithe/specsvalidator/apispecificationvalidator.py
@@ -10,12 +10,10 @@ IGNORED_ATTRIBUTES = ["parentType", "lastUpdatedBy", "externalID", "lastUpdatedD
 
 class APISpecificationValidator:
     """ Valdidate a candidate spec file against a spec file
-
     """
 
     def __init__(self, specification, candidate):
         """ Initialize the validator
-
         """
         self.candidate          = candidate
         self.specification      = specification
@@ -25,69 +23,37 @@ class APISpecificationValidator:
 
     def run(self):
         """ Run the validation
-
         """
-        self.validate_attributes_definition()
-        self.validate_parent_apis_definition()
-        self.validate_self_api_definition()
+        self.validate_attributes_declarations()
+        self.validate_parent_apis_declarations()
+        self.validate_self_api_declarations()
 
 
     def _append_validation_error(self, report, key, error_type, error):
-
-        if not key in report:
-            report[key] = {}
-
-        if not error_type in report[key]:
-            report[key][error_type] = []
-
+        """ Append a new reporting in the given report
+        """
+        if not key in report: report[key] = {}
+        if not error_type in report[key]: report[key][error_type] = []
         report[key][error_type].append(error)
+
+
+    def _check_similarities(self, word, corpus):
+        """ Find similarity of a given word from an array or words
+        """
+        search_list = {}
+        for key in corpus: search_list[key.lower()] = key
+        potential_matches = difflib.get_close_matches(word.lower(), search_list.keys())
+        return [search_list[key] for key in potential_matches] if len(potential_matches) else None
 
 
     ## Attributes Validation
 
-    def _validate_candidate_attribute(self, specification_attribute_definition, candidate_attribute_definition, attr_name, characteristic):
-        """ Validate given characteristic in the candidate information
-
-        """
-
-        if not characteristic in candidate_attribute_definition:
-            err = APISpecAttributeMissingCharacteristicException(attribute_name=attr_name, characteristic=characteristic)
-            self._append_validation_error(self.attribute_errors, attr_name, "missing_characteristics", err)
-            return False
-
-        if candidate_attribute_definition[characteristic] != specification_attribute_definition[characteristic]:
-            err = APISpecAttributeCharacteristicException(attribute_name=attr_name,
-                                                        characteristic=characteristic,
-                                                        expected_value=specification_attribute_definition[characteristic],
-                                                        actual_value=candidate_attribute_definition[characteristic])
-
-            self._append_validation_error(self.attribute_errors, attr_name, "characteristic_errors", err)
-            return False
-
-        elif characteristic is "allowedChoices" and specification_attribute_definition["type"] == "enum":
-
-            specification_choices      = ", ".join(sorted(specification_attribute_definition[characteristic]))
-            candidate_choices = ", ".join(sorted(candidate_attribute_definition[characteristic]))
-
-            if candidate_choices != specification_choices:
-                err = APISpecAttributeCharacteristicException(attribute_name=attr_name,
-                                                            characteristic=characteristic,
-                                                            expected_value=specification_attribute_definition[characteristic],
-                                                            actual_value=candidate_attribute_definition[characteristic])
-
-                self._append_validation_error(self.attribute_errors, attr_name, "characteristic_errors", err)
-
-                return False
-
-        return True
-
-
-    def validate_attributes_definition(self):
+    def validate_attributes_declarations(self):
         """ Validate a attributes definition
-
         """
         specification_attributes_definition = self.specification["model"]["attributes"]
         candidate_attributes_definition     = self.candidate["model"]["attributes"]
+        mispellings                         = []
 
         ## check for missing attributes or wrong characteristic
         for attribute_name in specification_attributes_definition:
@@ -97,25 +63,38 @@ class APISpecificationValidator:
 
             if not attribute_name in candidate_attributes_definition:
 
-                search_list = {}
-                for key in candidate_attributes_definition.keys():
-                    search_list[key.lower()] = key
+                similarities = self._check_similarities(attribute_name, candidate_attributes_definition.keys())
 
-                potential_matches = difflib.get_close_matches(attribute_name, search_list.keys())
-                final_matches = [search_list[key] for key in potential_matches]
-                if len(potential_matches):
-                    err = APISpecAttributeMispelledDeclarationException(attribute_name=attribute_name, potential_attributes=final_matches)
-                    self._append_validation_error(self.attribute_errors, attribute_name, "mispelling_errors", err)
+                if similarities:
+                    err = MispelledDeclarationException(declaration_name=attribute_name, potential_declarations=similarities)
+                    self._append_validation_error(self.attribute_errors, attribute_name, "mispelled_declarations", err)
+                    mispellings.extend(similarities)
                 else:
-                    err = APISpecAttributeMissingDeclarationException(attribute_name=attribute_name)
-                    self._append_validation_error(self.attribute_errors, attribute_name, "missing_attributes", err)
+                    err = MissingDeclarationException(declaration_name=attribute_name)
+                    self._append_validation_error(self.attribute_errors, attribute_name, "missing_declarations", err)
 
             else:
+
                 specification_attribute_definition = specification_attributes_definition[attribute_name]
                 candidate_attribute_definition     = candidate_attributes_definition[attribute_name]
 
                 for characteristic in specification_attribute_definition:
-                    self._validate_candidate_attribute(specification_attribute_definition, candidate_attribute_definition, attribute_name, characteristic)
+
+                    if not characteristic in candidate_attribute_definition:
+                        err = CharacteristicMissingException(characteristic_name=characteristic)
+                        self._append_validation_error(self.attribute_errors, attribute_name, "missing_characteristics", err)
+
+                    elif candidate_attribute_definition[characteristic] != specification_attribute_definition[characteristic]:
+                        err = CharacteristicMismatchException(characteristic_name=characteristic, expected_value=specification_attribute_definition[characteristic], actual_value=candidate_attribute_definition[characteristic])
+                        self._append_validation_error(self.attribute_errors, attribute_name, "characteristic_mismatches", err)
+
+                    elif characteristic is "allowedChoices" and specification_attribute_definition["type"] == "enum":
+                        specification_choices = ", ".join(sorted(specification_attribute_definition[characteristic]))
+                        candidate_choices     = ", ".join(sorted(candidate_attribute_definition[characteristic]))
+
+                        if candidate_choices != specification_choices:
+                            err = CharacteristicMismatchException(characteristic_name=characteristic, expected_value=specification_attribute_definition[characteristic], actual_value=candidate_attribute_definition[characteristic])
+                            self._append_validation_error(self.attribute_errors, attribute_name, "characteristic_mismatches", err)
 
         ## Check for extra attribiutes in candidate spec
         for attribute_name in candidate_attributes_definition:
@@ -123,63 +102,65 @@ class APISpecificationValidator:
             if attribute_name in IGNORED_ATTRIBUTES:
                 continue
 
+            if attribute_name in mispellings:
+                continue
+
             if not attribute_name in specification_attributes_definition:
-                err = APISpecAttributeExtraDeclarationException(attribute_name=attribute_name)
-                self._append_validation_error(self.attribute_errors, attribute_name, "extra_attributes_errors", err)
+
+                err = ExtraDeclarationException(declaration_name=attribute_name)
+                self._append_validation_error(self.attribute_errors, attribute_name, "extra_declarations", err)
 
 
     ## APIs Validation
 
-    def _validate_candidate_api(self, api_path, specification_parent_api_definition, candidate_parent_api_definition, target_reports):
-        """ Check all methods are correct
-
+    def _validate_api_declarations(self, specification_api_declarations, candidate_api_declarations, target_report):
+        """ Perform validations on api declarations
         """
-        specification_methods = ", ".join([operation["method"] for operation in sorted(specification_parent_api_definition["operations"])])
-        candidate_methods     = ", ".join([operation["method"] for operation in sorted(candidate_parent_api_definition["operations"])])
+        mispellings = []
 
-        if candidate_methods != specification_methods:
-            err = APISpecAPIMissingMethodException(api_path=api_path, expected_methods=specification_methods, actual_methods=candidate_methods)
-            self._append_validation_error(target_reports, api_path, "method_errors", err)
-            return False
+        ## check for missing apis or wrong operations
+        for api_path in specification_api_declarations:
 
-        return True
+            if not api_path in candidate_api_declarations:
 
-    def validate_self_api_definition(self):
+                similarities = self._check_similarities(api_path, candidate_api_declarations.keys())
+
+                if similarities:
+                    err = MispelledDeclarationException(declaration_name=api_path, potential_declarations=similarities)
+                    self._append_validation_error(target_report, api_path, "mispelled_declarations", err)
+                    mispellings.extend(similarities)
+                else:
+                    err = MissingDeclarationException(declaration_name=api_path)
+                    self._append_validation_error(target_report, api_path, "missing_declarations", err)
+
+            else:
+
+                specification_methods = ", ".join([operation["method"] for operation in sorted(specification_api_declarations[api_path]["operations"])])
+                candidate_methods     = ", ".join([operation["method"] for operation in sorted(candidate_api_declarations[api_path]["operations"])])
+
+                if candidate_methods != specification_methods:
+                    err = CharacteristicMismatchException(declaration_name="methods", expected_value=specification_methods, actual_value=candidate_methods)
+                    self._append_validation_error(target_report, api_path, "characteristic_mismatches", err)
+
+        ## check for extra apis
+        for api_path in candidate_api_declarations:
+
+            print mispellings
+            if api_path in mispellings:
+                continue
+
+            if not api_path in specification_api_declarations:
+                err = ExtraDeclarationException(declaration_name=api_path)
+                self._append_validation_error(target_report, api_path, "extra_declarations", err)
+
+    def validate_self_api_declarations(self):
         """ Validate self API information
-
         """
-        specification_self_apis_definition = self.specification["apis"]["self"]
-        candidate_self_apis_definition     = self.candidate["apis"]["self"]
-
-        for api_path in specification_self_apis_definition:
-
-            if not api_path in candidate_self_apis_definition:
-                err = APISpecAPIMissingException(api_path)
-                self._append_validation_error(self.self_api_errors, api_path, "missing_apis", err)
-
-            else:
-                specification_self_api_definition = specification_self_apis_definition[api_path]
-                candidate_self_api_definition     = candidate_self_apis_definition[api_path]
-
-                self._validate_candidate_api(api_path, specification_self_api_definition, candidate_self_api_definition, self.self_api_errors)
+        self._validate_api_declarations(self.specification["apis"]["self"], self.candidate["apis"]["self"], self.self_api_errors)
 
 
-    def validate_parent_apis_definition(self):
+    def validate_parent_apis_declarations(self):
         """ Validate Parent API information
-
         """
-        specification_parent_apis_definition = self.specification["apis"]["parents"]
-        candidate_parent_apis_definition     = self.candidate["apis"]["parents"]
-
-        for api_path in specification_parent_apis_definition:
-
-            if not api_path in candidate_parent_apis_definition:
-                err = APISpecAPIMissingException(api_path)
-                self._append_validation_error(self.parent_api_errors, api_path, "missing_apis", err)
-
-            else:
-                specification_parent_api_definition = specification_parent_apis_definition[api_path]
-                candidate_parent_api_definition     = candidate_parent_apis_definition[api_path]
-
-                self._validate_candidate_api(api_path, specification_parent_api_definition, candidate_parent_api_definition, self.parent_api_errors)
+        self._validate_api_declarations(self.specification["apis"]["parents"], self.candidate["apis"]["parents"], self.parent_api_errors)
 
--- monolithe/specsvalidator/exceptions.py
+++ monolithe/specsvalidator/exceptions.py
@@ -1,98 +1,29 @@
 # -*- coding: utf-8 -*-
 
-class APISpecAttributeCharacteristicException:
-    """ Represents an API attribute definition validation error
+class CharacteristicMismatchException:
+    def __init__(self, characteristic_name, expected_value, actual_value):
+        self.expected_value      = expected_value
+        self.actual_value        = actual_value
+        self.characteristic_name = characteristic_name
 
-    """
+class MispelledDeclarationException:
+    def __init__(self, declaration_name, potential_declarations):
+        self.declaration_name       = declaration_name
+        self.potential_declarations = potential_declarations
 
-    def __init__(self, characteristic, attribute_name, expected_value=None, actual_value=None):
-        self.attribute_name = attribute_name
-        self.expected_value = expected_value
-        self.actual_value   = actual_value
-        self.characteristic = characteristic
-        self.name           = "Attribute Characteristic Error"
+class CharacteristicMissingException:
+    def __init__(self, characteristic_name):
+        self.characteristic_name = characteristic_name
 
-    def __str__(self):
-        return "%s: '%s' should be '%s' but is '%s'" % (self.attribute_name, self.characteristic, self.expected_value, self.actual_value)
+class MissingDeclarationException:
+    def __init__(self, declaration_name):
+        self.declaration_name = declaration_name
 
-class APISpecAttributeMispelledDeclarationException:
-    """ Represents an API attribute capitalization error
+class ExtraDeclarationException:
+    def __init__(self, declaration_name):
+        self.declaration_name  = declaration_name
 
-    """
-
-    def __init__(self, attribute_name, potential_attributes):
-        self.attribute_name       = attribute_name
-        self.name                 = "Mispell Attribute Error"
-        self.potential_attributes = potential_attributes
-
-    def __str__(self):
-        return "%s seems to be mispelled. Potenial solutions: %s" % (self.attribute_name, self.potential_attributes)
-
-
-class APISpecAttributeMissingCharacteristicException:
-    """ Represents an API definition missing info characteristic error
-
-    """
-
-    def __init__(self, characteristic, attribute_name):
-        self.attribute_name = attribute_name
-        self.characteristic = characteristic
-        self.name           = "Missing Characteristic Error"
-
-    def __str__(self):
-        return "%s: information characteristic '%s' is missing" % (self.attribute_name, self.characteristic)
-
-
-class APISpecAttributeMissingDeclarationException:
-    """ Represents an API attribute missing error
-
-    """
-
-    def __init__(self, attribute_name):
-        self.attribute_name = attribute_name
-        self.name           = "Missing Attribute Error"
-
-    def __str__(self):
-        return "%s: is missing" % (self.attribute_name)
-
-
-class APISpecAttributeExtraDeclarationException:
-    """ Represents an API extra attribute error
-
-    """
-
-    def __init__(self, attribute_name):
-        self.attribute_name       = attribute_name
-        self.name                 = "Extra Attribute Error"
-
-    def __str__(self):
-        return "%s: should not exist" % (self.attribute_name)
-
-
-
-class APISpecAPIMissingException:
-    """ Represents an API path missing error
-
-    """
-
-    def __init__(self, api_name):
-        self.api_name = api_name
-        self.name     = "Missing API Error"
-
-    def __str__(self):
-        return "%s: missing" % (self.api_name)
-
-
-class APISpecAPIMissingMethodException:
-    """ Represents an API missing method error
-
-    """
-
-    def __init__(self, api_path, expected_methods, actual_methods):
-        self.api_path = api_path
+class MethodMismatchException:
+    def __init__(self, expected_methods, actual_methods):
         self.expected_methods = expected_methods
-        self.actual_methods = actual_methods
-        self.name     = "Missing API Method"
-
-    def __str__(self):
-        return "%s: methods should be '%s' but is '%s'" % (self.api_path, self.expected_methods, self.actual_methods)
+        self.actual_methods   = actual_methods
