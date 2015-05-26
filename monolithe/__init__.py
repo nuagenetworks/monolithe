b2c7d9581a4937ba16cd868c5a2fea197f3334b3
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-05-26 10:54:45 -0700

    uncomment this warning thing

--- monolithe/__init__.py
+++ monolithe/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# import requests
-# from requests.packages.urllib3.exceptions import InsecureRequestWarning
-# requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-# requests.packages.urllib3.disable_warnings()
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings()
 
 
 ## Monkey patch to use PROTOCOL_TLSv1 by default in requests
