f6a644db70a9811b9c195e29bba7d899bcc55f0c
Christophe Serafin <christophe.serafin@gmail.com>
2015-05-26 10:39:57 -0700

    Added specifications folder

---
+++ monolithe/courgette/courgette.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+from monolithe.lib.utils.printer import Printer
+
+from monolithe.lib.parsers import SwaggerParser
+from monolithe.lib.transformers import SpecificationTransformer, SwaggerTransformer
+from monolithe.validators.apivalidator.lib import TestsRunner
+
+
+class Courgette(object):
+    """ Validates API by launching specification-generated tests
+
+    """
+    def __init__(self, vsdurl, swagger_path, username, password, enterprise, apiversion, data):
+        """ Initializes a VSDKGenerator
+
+            Can be used to generate a vsdk from a remote vsdurl or a local swagger_path.
+
+            Args:
+                vsdurl (string): the url of the vsd with its port
+                username (string): the username to launch tests
+                password (string): the password to connect to the vsd
+                enterprise (string): the name of the enterprise to connect to the vsd
+                version (float): the version of the API to connect
+                data (dict): a dictionary containing following information:
+
+                    spec (dict): the specification
+                    parent (dict): the parent information (resourceName and ID)
+                    default_values: the default values for the object
+                    RESTName (string): Optionnally to avoid to provide the specification
+
+        """
+        self.rest_name = None
+        self.specification = None
+        self.vsdurl = vsdurl
+        self.swagger_path = swagger_path
+        self.username = username
+        self.password = password
+        self.enterprise = enterprise
+        self.apiversion = apiversion
+
+        if self.vsdurl is None and self.path is None:
+            Printer.raiseError("Please provide a vsd url or a path to swagger json file")
+
+        # Verify Parent information
+        if 'parentObject' not in data:
+            Printer.raiseError('No parentObject information provided')
+        elif 'resourceName' not in data['parentObject']:
+            Printer.raiseError('No parent resourceName information provided')
+        elif 'id' not in data['parentObject']:
+            Printer.raiseError('No parent id information provided')
+
+        self.parent_id = data['parentObject']['id']
+        self.parent_resource = data['parentObject']['resourceName']
+
+        # Verify default values
+        if 'defaultValues' not in data:
+            Printer.raiseError('No defaultValues information provided')
+
+        self.default_values = data['defaultValues']
+
+        if 'spec' in data:
+            self.specifications = data['spec']
+
+        if self.specification is None or len(self.specification) == 0:
+            if 'RESTName' not in data:
+                Printer.raiseError('No RESTName information provided')
+
+            self.specification = self.get_specification(rest_name=data['RESTName'])
+            self.rest_name = self.specification['model']['RESTName']
+
+    def run(self):
+        """ Run all tests
+
+            Returns:
+                A dictionnary containing tests' results.
+
+        """
+        processed_spec = SpecificationTransformer.get_objects(specifications={self.rest_name: self.specification})
+        model = processed_spec[self.rest_name]
+
+        runner = TestsRunner(vsdurl=self.vsdurl, username=self.username, password=self.password, enterprise=self.enterprise, version=self.apiversion, model=model, parent_resource=self.parent_resource, parent_id=self.parent_id, **self.default_values)
+
+        return runner.run()
---
+++ monolithe/courgette/lib/__init__.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+
+from unittest2 import TestSuite
+
+from monolithe.lib.factory import VSDKFactory
+from monolithe.lib.utils.constants import Constants
+from monolithe.lib.utils.vsdk import VSDKUtils
+
+from monolithe.courgette.lib.helpers import TestHelper
+from monolithe.courgette.lib.makers import GetTestMaker, CreateTestMaker, UpdateTestMaker, DeleteTestMaker, GetAllTestMaker
+from monolithe.courgette.lib.unittest import _MonolitheTestRunner
+
+
+class TestsRunner(object):
+    """ Runner for VSD Objects tests
+
+    """
+
+    def __init__(self, vsdurl, username, password, enterprise, version, model, parent_resource=None, parent_id=None, **default_values):
+        """ Initializes the TestsRunner.
+
+            Args:
+                vsdurl (string): the vsd url
+                username (string): the username to connect with
+                password (string): the password for the username
+                enterprise (string): the enterprise
+                version (float): the vsd api version (eg 3.2)
+                model (Model): the model representation of the object to test
+                parent_resource: the parent_resource if necessary
+                parent_id: the parent id if necessary
+                default_values: all default values to have a valid version of the model
+
+        """
+        VSDKFactory.init(version)
+        vsdk = VSDKFactory.get_vsdk_package()
+        TestHelper.use_vsdk(vsdk)
+
+        session = vsdk.NUVSDSession(api_url=vsdurl, username=username, password=password, enterprise=enterprise, version=version)
+        session.start()
+
+        self.user = session.user
+        self.resource_name = model.resource_name
+
+        python_attributes = {VSDKUtils.get_python_name(name): value for name, value in default_values.iteritems()}
+        self.vsdobject = VSDKFactory.get_instance_from_model(model, **python_attributes)
+
+        self.parent = None
+
+        if parent_resource and parent_id:
+            try:
+                self.parent = VSDKFactory.get_instance(parent_resource, id=parent_id)
+                self.parent.fetch()
+            except:
+                raise AttributeError('Could not find parent %s with ID=%s' % (parent_resource, parent_id))
+
+        VSDKFactory.update_fetchers_for_object(self.parent, self.vsdobject, model)
+
+        self.is_create_allowed = False
+        self.is_delete_allowed = False
+        self.is_get_allowed = False
+        self.is_get_all_allowed = False
+        self.is_update_allowed = False
+
+        for path, api in model.apis['parents'].iteritems():
+            for operation in api.operations:
+                method = operation.method
+                # Printer.log('%s %s' % (method, path))
+                if method == Constants.HTTP_METHOD_POST:
+                    self.is_create_allowed = True
+                elif method == Constants.HTTP_METHOD_GET:
+                    self.is_get_all_allowed = True
+
+        for path, api in model.apis['self'].iteritems():
+            for operation in api.operations:
+                method = operation.method
+                # Printer.log('%s' % (method))
+                if method == Constants.HTTP_METHOD_UPDATE:
+                    self.is_update_allowed = True
+                elif method == Constants.HTTP_METHOD_DELETE:
+                    self.is_delete_allowed = True
+                elif method == Constants.HTTP_METHOD_GET:
+                    self.is_get_allowed = True
+
+    def suite(self):
+        """ Returns a TestSuite that can be run
+
+            TestSuite is computed according to what is defined in the model
+
+        """
+        all_suites = TestSuite()
+
+        if self.is_create_allowed:
+            maker = CreateTestMaker(self.parent, self.vsdobject, self.user)
+            suite = maker.suite()
+            all_suites.addTests(suite)
+
+        if self.is_update_allowed:
+            maker = UpdateTestMaker(self.parent, self.vsdobject, self.user)
+            suite = maker.suite()
+            all_suites.addTests(suite)
+
+        if self.is_delete_allowed:
+            maker = DeleteTestMaker(self.parent, self.vsdobject, self.user)
+            suite = maker.suite()
+            all_suites.addTests(suite)
+
+        if self.is_get_allowed:
+            maker = GetTestMaker(self.parent, self.vsdobject, self.user)
+            suite = maker.suite()
+            all_suites.addTests(suite)
+
+        if self.is_get_all_allowed:
+            maker = GetAllTestMaker(self.parent, self.vsdobject, self.user)
+            suite = maker.suite()
+            all_suites.addTests(suite)
+
+        return all_suites
+
+    def run(self):
+        """ Runs all tests on the specified VSD
+
+        """
+        suite = self.suite()
+        results = _MonolitheTestRunner().run(suite)
+
+        return results
