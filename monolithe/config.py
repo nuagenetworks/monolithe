68b40330daba10810a7a0259bbf10c18806159f6
Christophe Serafin <christophe.serafin@gmail.com>
2015-09-17 14:41:52 -0700

    Removed mapping_path from config.ini

--- monolithe/config.py
+++ monolithe/config.py
@@ -35,16 +35,20 @@ class MonolitheConfig(object):
         self.config = ConfigParser()
         self.config.read(path)
 
-        # mapping
-        mapping_path = self.get_option("mapping_path")
-        self._check_path_exists(mapping_path)
-        self.mapping = ConfigParser()
-        self.mapping.read(mapping_path)
-
         # vanilla
         self.sdk_user_vanilla = self.get_option("sdk_user_vanilla", "sdk")
         self.apidoc_user_vanilla = self.get_option("apidoc_user_vanilla", "apidoc")
         self.sdkdoc_user_vanilla = self.get_option("sdkdoc_user_vanilla", "sdkdoc")
+
+        # mapping
+        mapping_path = '%s/mapping.ini' % os.path.dirname(path)
+
+        if not os.path.exists(path):
+            return
+
+        self.mapping = ConfigParser()
+        self.mapping.read(mapping_path)
+
 
     def get_sdk_config(self):
         """
@@ -74,7 +78,7 @@ class MonolitheConfig(object):
     def map_attribute(self, remote_name, attribute_name):
         """
         """
-        if not self.mapping.has_section(remote_name) or not self.mapping.has_option(remote_name, attribute_name):
+        if self.mapping is None or not self.mapping.has_section(remote_name) or not self.mapping.has_option(remote_name, attribute_name):
             return attribute_name
 
         return self.mapping.get(remote_name, attribute_name)
