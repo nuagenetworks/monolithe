f4aefdbe64e8301b49013eaf9d78da1fd139ae77
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-09-22 14:21:35 -0700

    Add a new FolderManager
    This class allows to get specifications info from a folder instead of a
    Github repository

---
+++ monolithe/specifications/foldermanager.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+
+from .specification import Specification
+
+
+class FolderManager (object):
+    """ RepositoryManager is an object that allows to manipulate the API specification repository
+    """
+
+    def __init__(self, folder, monolithe_config):
+        """
+        """
+        self._monolithe_config = monolithe_config
+        self._folder = folder;
+
+    def get_available_specifications(self):
+        """ Returns the list of available specification files
+
+            Args:
+                branch: the branch where to find files (default: "master")
+
+            Returns:
+                list of all available specification files in the given branch
+        """
+        ret = []
+        for filename in os.listdir(self._folder):
+            if os.path.splitext(filename)[1] != ".spec":
+                continue
+            ret.append(filename)
+        return ret
+
+    def get_api_version(self):
+        """
+        """
+        with open("%s/api.version" % self._folder, "r") as f:
+            return f.read().replace("\n", "").replace("\r", "").replace(" ", "")
+
+    def get_all_specifications(self):
+        """
+        """
+        specifications = []
+        for name in self.get_available_specifications():
+            specifications.append(self.get_specification(name))
+        return specifications
+
+    def get_specification_data(self, name):
+        """
+        """
+        with open("%s/%s" % (self._folder, name), "r") as f:
+            return json.loads(f.read())
+
+    def get_specification(self, name):
+        """
+        """
+        return Specification(data=self.get_specification_data(name), monolithe_config=self._monolithe_config)
+
+    def get_specifications(self, names, callback=None):
+        """
+        """
+        specifications = []
+        for name in names:
+            specification.append(Specification(data=self.get_specification_data(name=name)))
+        return specifications
