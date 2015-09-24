e4d8a0c3e7af259388d38727b7d68fcba4e66c1f
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-09-24 12:28:43 -0700

    fix class methods

--- monolithe/courgette/lib/helper.py
+++ monolithe/courgette/lib/helper.py
@@ -10,21 +10,10 @@ class TestHelper(object):
     """ Helper to make tests easier
 
     """
-    _sdk = None
-    _debug = False
-    _session_class_name = None
-
-    @classmethod
-    def set_debug_mode(cls, debug=True):
-        """ Activate debug mode
-
-        """
-        cls._debug = debug
-
-        if cls._debug:
-            cls._sdk.utils.set_log_level(logging.DEBUG)
-        else:
-            cls._sdk.utils.set_log_level(logging.ERROR)
+    def __init__(self):
+       self._sdk = None
+       self._debug = False
+       self._session_class_name = None
 
     @classmethod
     def trace(cls, connection):
@@ -48,37 +37,33 @@ class TestHelper(object):
             Printer.log("Errors")
             Printer.json(response.errors)
 
-    @classmethod
-    def use_sdk(cls, sdk, session_class_name):
+    def use_sdk(self, sdk, session_class_name):
         """ Retain used sdk
 
         """
-        cls._session_class_name = session_class_name
-        cls._sdk = sdk
+        self._session_class_name = session_class_name
+        self._sdk = sdk
 
-    @classmethod
-    def current_push_center(cls, session_class_name):
+    def current_push_center(self, session_class_name):
         """ Get current push center
 
         """
 
-        session = getattr(cls._sdk, cls._session_class_name).get_current_session()
+        session = getattr(self._sdk, self._session_class_name).get_current_session()
         return session.push_center
 
-    @classmethod
-    def set_api_key(cls, api_key):
+    def set_api_key(self, api_key, sdk_object=None):
         """ Change api key
 
         """
-        session = getattr(cls._sdk, cls._session_class_name).get_current_session()
+        session = getattr(self._sdk, self._session_class_name).get_current_session()
         session.login_controller.api_key = api_key
 
-    @classmethod
-    def session_headers(cls):
+    def session_headers(self):
         """ Get headers
 
         """
-        session = getattr(cls._sdk, cls._session_class_name).get_current_session()
+        session = getattr(self._sdk, self._session_class_name).get_current_session()
         controller = session.login_controller
 
         headers = dict()
@@ -88,42 +73,37 @@ class TestHelper(object):
 
         return headers
 
-    @classmethod
-    def send_request(cls, method, url, data=None, remove_header=None):
+    def send_request(self, method, url, data=None, remove_header=None):
         """ Send request with removed header
 
         """
-        headers = cls.session_headers()
+        headers = self.session_headers()
 
         if remove_header:
             headers.pop(remove_header)
 
         return requests.request(method=method, url=url, data=data, verify=False, headers=headers)
 
-    @classmethod
-    def send_post(cls, url, data, remove_header=None):
+    def send_post(self, url, data, remove_header=None):
         """ Send a POST request
 
         """
-        return cls.send_request(method="post", url=url, data=data, remove_header=remove_header)
+        return self.send_request(method="post", url=url, data=data, remove_header=remove_header)
 
-    @classmethod
-    def send_put(cls, url, data, remove_header=None):
+    def send_put(self, url, data, remove_header=None):
         """ Send a PUT request
 
         """
-        return cls.send_request(method="put", url=url, data=data, remove_header=remove_header)
+        return self.send_request(method="put", url=url, data=data, remove_header=remove_header)
 
-    @classmethod
-    def send_delete(cls, url, data, remove_header=None):
+    def send_delete(self, url, data, remove_header=None):
         """ Send a DELETE request
 
         """
-        return cls.send_request(method="delete", url=url, data=data, remove_header=remove_header)
+        return self.send_request(method="delete", url=url, data=data, remove_header=remove_header)
 
-    @classmethod
-    def send_get(cls, url, remove_header=None):
+    def send_get(self, url, remove_header=None):
         """ Send a GET request
 
         """
-        return cls.send_request(method="get", url=url, remove_header=remove_header)
+        return self.send_request(method="get", url=url, remove_header=remove_header)
