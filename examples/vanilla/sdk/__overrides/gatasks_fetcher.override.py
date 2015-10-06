d72ccff7fd95531239db1b3b68c46213a8efc112
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-10-05 17:04:50 -0700

    improve example

---
+++ examples/vanilla/sdk/__overrides/gatasks_fetcher.override.py
@@ -0,0 +1,5 @@
+def is_this_test_usesless(self):
+    """
+    Just an example
+    """
+    return True
---
+++ examples/vanilla/sdk/__overrides/gatdlsession.override.py
@@ -0,0 +1,7 @@
+@property
+def supercalifragilisticexpialidocious(self):
+    """
+        For obvious simplicity, you can use this property to get
+        the root object
+    """
+    return self.root_object()
