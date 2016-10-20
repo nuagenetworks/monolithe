4c7f5a887efdbcaf9e4bb6a6039aa491c3021193
Corentin Henry <corentinhenry@gmail.com>
2016-10-20 09:14:15 -0700

    bump version to 1.1.3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     name="monolithe",
     packages=find_packages(exclude=["*tests*"]),
     include_package_data=True,
-    version="1.1.2",
+    version="1.1.3",
     description="Monolithe is a sdk generator",
     author="Christophe Serafin, Antoine Mercadal",
     author_email="christophe.serafin@nuagenetworks.net, antoine@nuagenetworks.net",
