8105c57c57dbce8ee1b1bbe99868642ebc6d84fc
Philippe Dellaert <philippe@dellaert.org>
2017-10-30 00:33:14 +0400

    Updating version for latest csharp support for nuget

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     name="monolithe",
     packages=find_packages(exclude=["*tests*"]),
     include_package_data=True,
-    version="1.4.0",
+    version="1.4.1",
     description="Monolithe is a sdk generator",
     author="Nuage Networks",
     author_email="opensource@nuagnetworks.net",
