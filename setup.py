4f8419d34deffa7f87e5a1c44f643fd53370028e
Philippe Dellaert <philippe@dellaert.org>
2018-01-03 13:36:28 +0100

    Fixing nuagenetworks/vsd-api-specifications#49

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     name="monolithe",
     packages=find_packages(exclude=["*tests*"]),
     include_package_data=True,
-    version="1.4.1",
+    version="1.4.2",
     description="Monolithe is a sdk generator",
     author="Nuage Networks",
     author_email="opensource@nuagnetworks.net",
